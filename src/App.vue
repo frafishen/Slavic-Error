a91179134811ddf947f47c43586de1ee8a7e18d1
Shen Jenny <frafishen@gmail.com>
2023-02-17 00:35:12 +0800

    Initialize

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,12 +15,178 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,700,700i,900&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+}
+
+header{
+    width: 100vw;
+    height: 60px;
+    border-bottom: solid 1px rgba(16, 16, 16, 0.1);
+    font-family: 'News Cycle', sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    position: fixed;
+    top:0;
+    z-index:5;
+
+}
+
+header .inner{
+    width: 100%;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+    background-color: #823737;
+    border: #823737;
+}
+
+header .logo{
+    display: table;
+    height: 100%;
+    float: left;
+}
+
+header .logo div{
+    height: 100%;
+    display: table-cell;
+    vertical-align: middle;
+}
+
+header .logo img{
+    height: 55px;
+    margin-top: 3px; /* Optical alignment of the logo */
+}
+
+header nav{
+    float: left;
+    margin-left: 5px;
+    height: 100%;
+}
+
+header nav li{
+    display: table;
+    height: 100%;
+    float: left;
+    margin-right: 30px;
+}
+
+header nav li:last-of-type{
+    margin-right: 0;
+}
+
+header nav span {
+    display: table-cell;
+    vertical-align: middle;
+    color: white;
+    font-family: 'News Cycle', sans-serif;
+    font-weight: bold;
+    font-size: 18px;
+}
+
+header nav a{
+    color: white;
+}
+
+body
+{
+    padding-top:  40px;
+    padding-bottom: 60px;
+}
+
+/* .switch {
+    position: relative;
+    display: inline-block;
+    margin-left: 20px;
+    /* margin-top: 10px; 
+    width: 60px;
+    height: 28px;
+} */
+  
+  /* Hide default HTML checkbox */
+/* .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+} */
+
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+  
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 22px;
+    width: 22px;
+    left: 3px;
+    bottom: 3px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+input:checked + .slider {
+    background-color: #52ce77;
+}
+
+input:focus + .slider {
+    box-shadow: 0 0 2px #ccc;
+}
+
+input:checked + .slider:before {
+    -webkit-transform: translateX(32px);
+    -ms-transform: translateX(32px);
+    transform: translateX(32px);
+}
+
+/* Rounded sliders */
+.slider.round {
+    border-radius: 34px;
+    box-shadow: 0 0 2px #ccc;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+    box-shadow: 0 0 2px #ccc;
+}
+
+
+#textarea_input{
+    width: 60%;
+    height: 250px;
+    /* border: none; */
+    /* outline: none; */
+    margin-top: 10px;
+    margin-left: 20px;
+    resize: none;
+    font-family: 'Inter';
+    font-size: 14px;
+    padding: 10px;
+    background-color: #f5f5f5;
+    color: #000;
+    border-radius: 5px;
+    box-shadow: 0 0 2px #ccc;
+}
+#toolbar{
+    margin-left: 20px;
+    /* margin-top: 5px; */
+    margin-bottom: 5px;
+    /* border-color: #000 */
+    /* border:1px solid black; */
+
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,161 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <head> 
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.1.0/css/buttons.dataTables.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=News+Cycle&amp;display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" href="logo.png">
+    </head>
+    <body>
+        <header>
+            <div class="inner">
+                <div class="logo">
+                    <div>
+                        <img src="logo.png" alt="logo">
+                    </div>
+
+                </div>
+                <nav>
+                    <li><span>LEARNER CORPUS OF SLAVIC LANGUAGES (RUSSIAN)</span></li>
+                    <li><span><a href="http://lcsl.nccu.edu.tw/"><i class="fas fa-home"></i> Home</a></span></li>
+                    <li><span><a href="http://lcsl.nccu.edu.tw:3838/sample-apps/searchCorpus/Russian.Rmd#section-search"><i class="fas fa-search"></i> Search</a></span></li>
+                    <li><span><a href="http://lcsl.nccu.edu.tw:3838/sample-apps/searchCorpus/Russian.Rmd#section-advanced-filter"><i class="fas fa-filter"></i> Advanced Filter</a></span></li>
+                    <li><span><a href="http://lcsl.nccu.edu.tw:8080/search/"><i class="fab fa-searchengin"></i> Lexical/Grammatical Search</a></span></li>
+                    <li><span><a href=""><i class="fab error-anno"></i> Error Annotation</a></span></li>
+                </nav>
+            </div>
+        </header>
+        <!-- <br> -->
+        <div>
+            <div id="toolbar">
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label>
+            <span id="mode">Original</span>
+            <button id="saveButton" @click="saveContent()"><img src="../../public/save.png" class="pic"/></button>
+            <button @click="transContent()"><img src="../../public/transfer-data.png" class="pic" /></button>
+            <button @click="preStep()"><img src="../../public/back-arrow.png"  class="pic" /></button>
+            <button @click="nextStep()"><img src="../../public/next.png"  class="pic" /></button>
+            </div>
+            <hr>
+            
+            
+            <textarea id="textarea_input" v-model="rawContent" v-if="view===0"></textarea>
+            <div id="selectArea" @mouseup="getSelection">
+                <label v-if="view===1">{{ori_content}}</label>
+            </div>
+        </div>
+        <footer class="bottomBar">
+            <b>NCCU LCSL © 2023.</b>
+        </footer>
+    </body>
+
 </template>
 
 <script>
+// import { ref } from 'vue'
+
+// let select_content = ref(''); //選取內容
+let selectionObj = window.getSelection();
+console.log(selectionObj.toString());
+
+function transContent() {
+    this.ori_content = this.rawContent;
+    this.view = 1;
+    console.log(this.view);
+
+}
+function saveContent() {
+
+}
+function preStep() {
+
+}
+function nextStep() {
+
+}
+function getSelection() {
+    const selection = window.getSelection();
+    const selectedText = selection.toString();
+    const words = selectedText.split(/\s+/);
+    const range = selection.getRangeAt(0);
+    const selectedWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (range.toString().indexOf(word) !== -1) {
+        selectedWords.push(word);
+    }
+    }
+
+    console.log(selectedWords); 
+}
 export default {
+
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data(){
+    return {
+        rawContent: '',
+        ori_content: '',
+        view: 0,
+    }
+  },
+    methods: {
+        transContent: transContent,
+        saveContent: saveContent,
+        preStep: preStep,
+        nextStep: nextStep,
+        getSelection: getSelection,
+    }
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#saveButton{
+    margin-left: 20px;
+    /* margin-top:5px; */
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#mode {
+    margin-left: 5px;
+    font-family: 'Inter';
+    font-size: 14px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#selectArea{
+    width: 60%;
+    height: 250px;
+    margin-top: 10px;
+    margin-left: 20px;
 }
-a {
-  color: #42b983;
+.pic{
+    width: 25px;
+    height: 25px;  
+}
+.bottomBar{
+    position:fixed; bottom:0; left: 0px; width:100%; height:22px;
+                color: #FFFFFF; padding: 1%; background-color: #A26969; font-family: 'News Cycle'; text-align:center;
+}
+.switch {
+    position: relative;
+    display: inline-block;
+    margin-left: 20px;
+    margin-top: 0px;
+    /* margin-bottom: 10px; */
+    width: 60px;
+    height: 28px;
+}
+  
+  /* Hide default HTML checkbox */
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
 }
 </style>
